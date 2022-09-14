1f9a0148fc83073e520092d857e20bb62be4491b
Emily <heyemmers@gmail.com>
2022-09-14 13:47:12 -0400

    welcome message on submit login button click

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,8 @@
 
 const logIn = document.getElementById("logIn");
 const logInContainerClose = document.getElementById("logInContainerClose");
+const logInSubmit = document.getElementById("submit");
+const welcomeContainer = document.getElementById("welcome");
 
 logIn.addEventListener("click", ()=>{
     document.getElementById("logInContainer").style.top = "17%";
@@ -12,4 +14,12 @@ logIn.addEventListener("click", ()=>{
 
 logInContainerClose.addEventListener("click", ()=>{
     document.getElementById("logInContainer").style.top = "-100vh";
+})
+
+logInSubmit.addEventListener("click", ()=> {
+    const email = document.getElementById("email").value;
+    welcomeContainer.innerHTML += "Welcome " + email;
+    document.getElementById("logInContainer").style.top = "-100vh";
+    
+
 })
