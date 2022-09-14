02145b26fafdeeffab66ca27cb208a21f83dca25
1Boody <109798316+1Boody@users.noreply.github.com>
2022-09-14 21:59:03 +0100

    established boilerplate for DB connection

---
+++ db.js
@@ -0,0 +1,14 @@
+const {Pool} = require('pg')
+
+
+// fill in the values accordingly to establish a DB connection
+const pool = new Pool({
+    user: '',
+    password: '',
+    host: '',
+    port: , 
+    database: ''
+})
+
+
+module.exports = {pool}
