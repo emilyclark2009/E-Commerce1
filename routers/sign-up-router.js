d5dced8a7575400f87ef7168a64ff39ba167038a
Scar3Cr0w <edgar.garibay22@gmail.com>
2022-09-14 21:32:19 -0700

    Added first and last name field, added req

--- routers/sign-up-router.js
+++ routers/sign-up-router.js
@@ -6,7 +6,7 @@ const signUpRouter = express.Router()
 
 // the sign up page doesn't ask for the firstname nor lastname (yet) - in the meantime i'll just use null as placeholders 
 signUpRouter.post('/sign-up', (req,res)=>{
-    pool.query('INSERT INTO Customers (password, first_name, last_name, email) VALUES($1,$2,$3,$4)', [req.body.newPassword, null , null , req.body.newUserName], (err,results)=>{
+    pool.query('INSERT INTO Customers (password, first_name, last_name, email) VALUES($1,$2,$3,$4)', [req.body.newPassword, [req.body.newFirstName], [req.body.newLastName] , req.body.newUserName], (err,results)=>{
         if (err){
             console.log(err)
         }
