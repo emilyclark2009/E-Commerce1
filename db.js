268718218d62aa181843601044f2b55c980cd8fe
1Boody <109798316+1Boody@users.noreply.github.com>
2022-09-16 20:11:50 +0100

    connected the server to the heroku DB

--- db.js
+++ db.js
@@ -1,16 +1,15 @@
-/*
+
 const {Pool} = require('pg')
 
 
 // fill in the values accordingly to establish a DB connection
 const pool = new Pool({
-    user: '',
-    password: '',
-    host: '',
-    port: '', 
-    database: ''
+    user: 'qluoipagapiyye',
+    password: '7deddd8775b44cfbcdaa549b40edfaa0257f687bbc159ed95dd10799a370a7fa',
+    host: 'ec2-34-231-42-166.compute-1.amazonaws.com',
+    port: '5432', 
+    database: 'd9b8rmbfobg3i6'
 })
 
 
 module.exports = {pool}
-*/
