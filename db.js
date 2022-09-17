ae146d54907da8da9a5750e53f8c5cf4de5bde14
1Boody <109798316+1Boody@users.noreply.github.com>
2022-09-17 20:35:55 +0100

    + replaced html w/ ejs + added sign up functionality (makes sure no duplicate emails are entered) + cleaned up some code

--- db.js
+++ db.js
@@ -1,14 +1,21 @@
 
 const {Pool} = require('pg')
 
+// const connectionString = `postgresql://qluoipagapiyye:7deddd8775b44cfbcdaa549b40edfaa0257f687bbc159ed95dd10799a370a7fa@5432/d9b8rmbfobg3i6`;
+
 
 // fill in the values accordingly to establish a DB connection
 const pool = new Pool({
+    //connectionString: connectionString,
     user: 'qluoipagapiyye',
-    password: '7deddd8775b44cfbcdaa549b40edfaa0257f687bbc159ed95dd10799a370a7fa',
-    host: 'ec2-34-231-42-166.compute-1.amazonaws.com',
-    port: '5432', 
-    database: 'd9b8rmbfobg3i6'
+  host: 'ec2-34-231-42-166.compute-1.amazonaws.com',
+  database: 'd9b8rmbfobg3i6',
+  password: '7deddd8775b44cfbcdaa549b40edfaa0257f687bbc159ed95dd10799a370a7fa',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
+
 })
 
 
